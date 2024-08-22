28e437fc64e69f64484abdc4c1dd8854080b557b
DAliRIJTK <148074211+DAliRIJTK@users.noreply.github.com>
2024-08-22 22:20:42 +0700

    Pertemuan 1

---
+++ Pertemuan 1/soal1.java
@@ -0,0 +1,10 @@
+public class soal1{
+	public static void main (String[] arg) {
+		byte angka1 = 125;
+		byte angka2 = 6;
+		byte hasil = (byte) (angka1+angka2);
+		
+		System.out.println("Hasil 1 " +hasil);
+	}
+}
+
